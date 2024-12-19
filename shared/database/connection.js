735353074cb5644b3b87c4ab3511001a8ce4cf1f
Saranjith <saranjith@metrictreelabs.com>
2024-12-19 22:43:43 +0530

    database: connection created

---
+++ shared/database/connection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL: " + err.stack);
+    return;
+  }
+  console.log("Connected to MySQL as ID " + db.threadId);
+});
